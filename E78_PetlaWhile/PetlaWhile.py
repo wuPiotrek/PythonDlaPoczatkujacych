1e73ddfc3cbd0f41b29d03a46be6c656d61cdb24
wuPiotrek <wupiotrek@gmail.com>
2018-11-06 22:53:52 +0100

    New excercises

---
+++ E78_PetlaWhile/PetlaWhile.py
@@ -0,0 +1,44 @@
+#################################################
+exNumber = 1
+print("-" * 5, exNumber, "-" * 30)
+# Code:
+
+firstRow = 1
+lastRow = 31
+currentRow = firstRow
+
+while currentRow <= lastRow:
+    print(currentRow)
+    currentRow += 1
+    
+#################################################
+exNumber = 2
+print("-" * 5, exNumber, "-" * 30)
+# Code:
+
+i = 1
+while i < 14:
+    print('i =', i, '\ti ^ 2 =', i ** 2, '\ti ^ 3 =', i ** 3)
+    i += 1
+
+#################################################
+exNumber = 3
+print("-" * 5, exNumber, "-" * 30)
+# Code:
+
+x = 0
+while x < 17:
+    print('x =', x, '\t2 ^ x =', 2 ** x)
+    x += 1
+    
+#################################################
+exNumber = 4
+print("-" * 5, exNumber, "-" * 30)
+# Code:
+
+z = 1
+while z < 11:
+    print(z * 'x')
+    z += 1
+
+#################################################

df603ca2b9bc92274d7d0da64a5fe06eb51c5f49
wuPiotrek <wupiotrek@gmail.com>
2018-11-07 23:32:03 +0100

    New excercises

---
+++ E96_PetlaForOkreslonaIloscRazy/PetlaForOkreslonaIloscRazy.py
@@ -0,0 +1,42 @@
+#################################################
+exNumber = 1
+print("-" * 5, exNumber, "-" * 30)
+# Code:
+
+string_A = '+---+---+---+---+'
+string_B = '|   |   |   |   |'
+
+for i in range(10):
+    print(string_A)
+
+#################################################
+exNumber = 2
+print("-" * 5, exNumber, "-" * 30)
+# Code:
+
+for i in range(4):
+    print(string_A)
+    print(string_B)
+else:
+    print(string_A)
+
+#################################################
+exNumber = 3
+print("-" * 5, exNumber, "-" * 30)
+# Code:
+
+for i in range(1, 10):
+    print('x' * i)
+
+#################################################
+exNumber = 4
+print("-" * 5, exNumber, "-" * 30)
+# Code:
+
+for i in range(1, 10):
+    if i % 2 != 0:
+        print('o'* i)
+    else:
+        print('x'* i)
+        
+#################################################   
